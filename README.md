8670a83649ded8e185d1508f5e5a35b93d52fc69
Ramon Viedma <ramon.viedmap@gmail.com>
2019-12-04 20:45:42 +0100

    Perception Exercise 1.3

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # webcam_point_features
-Detection of ORB features from online webcam imges.
+Detection of ORB features from online webcam images.
+
+## What is ORB?
+
+ORB is an algorithm used in computer vision for detecting local features, that is, interesting points within an image. Its acronym ORB stands for Oriented FAST and Rotated BRIEF. FAST (Features from Accelerated Segment Test) is also a keypoint detector based on a corner detection algorithm, specially designed to be computationally efficient. FAST is suitable for real-time video processing because of its performance.
+On the other hand, BRIEF (Binary Robust Independent Elementary Feature) is a feature descriptor. This means, it does not search keypoints in images, it just "describes" the detected keypoint.
+
+ORB was created mainly because other well-known algorithms, such as SIFT and SURF, are patented and therefore not free to use. So ORB is a combination of detecting keypoints in an image and then creating a descriptor for that point that is based on FAST and BRIEF, but with improved characteristics.
+
+## Sources:
+
+Wikipedia: https://en.wikipedia.org/wiki/Oriented_FAST_and_rotated_BRIEF
+
+Medium: https://medium.com/@deepanshut041/introduction-to-orb-oriented-fast-and-rotated-brief-4220e8ec40cf
+
+OpenCV: https://docs.opencv.org/master/dc/d7d/tutorial_py_brief.html
+
+Paper: http://www.willowgarage.com/sites/default/files/orb_final.pdf
+
+## Usage
+After compiling with the usual steps, you have two options:
+
+a) Run the real-time keypoint detector:
+```
+./point_features
+```
+
+b) Compare test images for the mask:
+```
+./compare_orb <input_image> <factor>
+```
+**input_image**: is the path to the image to load and test
+
+**factor**: a percent between 0.0 and 1.0 of the total image area to used for searching
+
+For example:
+
+```
+./compare_orb ../images/img1.jpg 0.5
+```
+
+## Masking
+You can limit the area to search for keypoints inside the image with the mask parameter. This is just a region of interest (ROI) and will discard the rest of the image. Here you can see 2 images when searching in the 100% of the image or only in the 25% of it.
+
+![Sample webcam image](images/mask_no_mask.png)
--- src/point_features.cpp
+++ src/point_features.cpp
@@ -10,70 +10,136 @@
 #include <vector>
 
 //consts
-const unsigned int MIN_NUM_FEATURES = 300; //minimum number of point fetaures
+const unsigned int MIN_NUM_FEATURES = 300; //minimum number of point features
+
+void compare_orb_masks(const char * filename, cv::Ptr<cv::ORB> & orb_detector, double factor)
+{
+    cv::Mat img1 = cv::imread( filename );
+    cv::Mat img2 = cv::imread( filename );
+
+    std::vector<cv::KeyPoint> point_set1; //set of point features
+    cv::Mat descriptor_set1; //set of descriptors, for each feature there is an associated descriptor
+
+    std::vector<cv::KeyPoint> point_set2; //set of point features
+    cv::Mat descriptor_set2; //set of descriptors, for each feature there is an associated descriptor
+
+    cv::Mat mask(cv::Mat::zeros(img2.size(),CV_8U));
+
+    mask(cv::Rect(0,0,img2.cols*factor,img2.rows*factor))=1;
+
+    orb_detector->detectAndCompute(img1, cv::noArray(), point_set1, descriptor_set1);
+    orb_detector->detectAndCompute(img2, mask, point_set2, descriptor_set2);
+
+    //draw points on the image
+    cv::drawKeypoints( img1, point_set1, img1, 255, cv::DrawMatchesFlags::DEFAULT );
+    cv::drawKeypoints( img2, point_set2, img2, 255, cv::DrawMatchesFlags::DEFAULT );
+
+
+    std::cout << "Factor: " << factor << std::endl;
+    std::cout << "Num points no mask: " << point_set1.size() << std::endl;
+    std::cout << "Num points with mask: " << point_set2.size() << std::endl;
+    //********************************************************************
+
+    //show image
+    cv::imshow("No Mask", img1);
+    cv::imshow("With Mask", img2);
+
+    cv::waitKey();
+}
 
 int main(int argc, char *argv[])
 {
-    cv::VideoCapture camera; //OpenCV video capture object
-    cv::Mat image; //OpenCV image object
-	int cam_id; //camera id . Associated to device number in /dev/videoX
+
     cv::Ptr<cv::ORB> orb_detector = cv::ORB::create(); //ORB point feature detector
     orb_detector->setMaxFeatures(MIN_NUM_FEATURES);
-    std::vector<cv::KeyPoint> point_set; //set of point features
-    cv::Mat descriptor_set; //set of descriptors, for each feature there is an associated descriptor
 
-	//check user args
-	switch(argc)
-	{
-		case 1: //no argument provided, so try /dev/video0
-			cam_id = 0;
-			break;
-		case 2: //an argument is provided. Get it and set cam_id
-			cam_id = atoi(argv[1]);
-			break;
-		default:
-			std::cout << "Invalid number of arguments. Call program as: webcam_capture [video_device_id]. " << std::endl;
-			std::cout << "EXIT program." << std::endl;
-			break;
-	}
+    std::string cmd(argv[0]);
+    if( cmd.find("compare_orb") != cmd.npos )
+    {
+      if(argc<2)
+      {
+          std::cerr << "Missing input image file\n";
+          return -1;
+      }
+      double factor = 1.0;
+      if(argc>2) factor = atof(argv[2]);
+      compare_orb_masks( argv[1], orb_detector, factor );
 
-	//advertising to the user
-	std::cout << "Opening video device " << cam_id << std::endl;
+      return 0;
+    }
+
+
+    cv::VideoCapture camera; //OpenCV video capture object
+    cv::Mat image; //OpenCV image object
+    int cam_id; //camera id . Associated to device number in /dev/videoX
+
+    //check user args
+    switch(argc)
+    {
+      case 1: //no argument provided, so try /dev/video0
+        cam_id = 0;
+        break;
+      case 2: //an argument is provided. Get it and set cam_id
+        cam_id = atoi(argv[1]);
+        break;
+      default:
+        std::cout << "Invalid number of arguments. Call program as: webcam_capture [video_device_id]. " << std::endl;
+        std::cout << "EXIT program." << std::endl;
+        break;
+    }
+
+    //advertising to the user
+    std::cout << "Opening video device " << cam_id << std::endl;
 
     //open the video stream and make sure it's opened
     if( !camera.open(cam_id) )
-	{
+    {
         std::cout << "Error opening the camera. May be invalid device id. EXIT program." << std::endl;
         return -1;
     }
 
     //Process loop. Capture and point feature extraction. User can quit pressing a key
     while(1)
-	{
-		//Read image and check it. Blocking call up to a new image arrives from camera.
+    {
+        //Read image and check it. Blocking call up to a new image arrives from camera.
         if(!camera.read(image))
-		{
+        {
             std::cout << "No image" << std::endl;
             cv::waitKey();
         }
 
-    //**************** Find ORB point fetaures and descriptors ****************************
+        //**************** Find ORB point fetaures and descriptors ****************************
+
+        std::vector<cv::KeyPoint> point_set; //set of point features
+        cv::Mat descriptor_set; //set of descriptors, for each feature there is an associated descriptor
 
         //clear previous points
-        point_set.clear();
+        //point_set.clear();
 
+        /* Detects keypoints and computes the descriptors
+        virtual void cv::Feature2D::detectAndCompute 	(
+            InputArray  	image,
+            InputArray  	mask,
+            std::vector< KeyPoint > &  	keypoints,
+            OutputArray  	descriptors,
+            bool  	useProvidedKeypoints = false
+          )
+        */
         //detect and compute(extract) features
+        cv::Mat mask(cv::Mat::ones(image.size(),CV_8U));
+
         orb_detector->detectAndCompute(image, cv::noArray(), point_set, descriptor_set);
+        //orb_detector->detectAndCompute(image, mask, point_set, descriptor_set);
 
         //draw points on the image
         cv::drawKeypoints( image, point_set, image, 255, cv::DrawMatchesFlags::DEFAULT );
 
-    //********************************************************************
+        //********************************************************************
 
         //show image
         cv::imshow("Output Window", image);
 
-		//Waits 30 millisecond to check if 'q' key has been pressed. If so, breaks the loop. Otherwise continues.
-    	if( (unsigned char)(cv::waitKey(30) & 0xff) == 'q' ) break;
+        //Waits 30 millisecond to check if 'q' key has been pressed. If so, breaks the loop. Otherwise continues.
+        if( (unsigned char)(cv::waitKey(30) & 0xff) == 'q' ) break;
     }   
 }
